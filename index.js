115904fc8dfce20ca134d8676e0b771ebc1c9868
Ramandeep <sep2001aryan>
2020-04-27 18:01:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+    let width = $(document).width();
+    if (width < 900) {
+        console.log("here i am");
+        $(".navbar").hide();
+        $(".hamburger").on("click", () => {
+            console.log("hello");
+            if ($(".navbar").css("display") == "none") {
+                $(".navbar").show("slow");
+            } else {
+                $(".navbar").hide("slow");
+            }
+            $(".navbar > ul > li > a").on("click", (event) => {
+                $(".navbar").hide("slow");
+            });
+        });
+    }
+    if (width > 900) {
+        $(".mobile-nav").hide();
+    }
+});
