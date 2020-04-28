8f1417eb992558f60e9a011add608d540fd540e6
Aryan <sep2001aryan@yahoo.com>
2020-04-29 01:42:04 +0530

    bug fixes

--- index.js
+++ index.js
@@ -18,5 +18,16 @@ $(document).ready(() => {
     if (width > 900) {
         $(".navbar").show();
         console.log("hide");
+        $(".hamburger").on("click", () => {
+            console.log("hello");
+            if ($(".navbar").css("display") == "none") {
+                $(".navbar").show("slow");
+            } else {
+                $(".navbar").hide("slow");
+            }
+            $(".navbar > ul > li > a").on("click", (event) => {
+                $(".navbar").hide("slow");
+            });
+        });
     }
 });
